9188f5c429a184830d3e0044ac04ed8f1409e049
bhavana <bhavana@tradelab.in>
2024-03-27 08:12:00 +0530

    Added base route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { config } from './Components/common/config';
 import Slider from './Components/Dashboard';
 import Login from './Components/Login';
@@ -9,6 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
         <Route path={"/login"} element= {<Login />} />
         <Route
           path="/dashboard"
